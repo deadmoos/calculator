96d544899e6a520113a5e966ab7202e8745e59d2
deadmoos <72154654+deadmoos@users.noreply.github.com>
2023-10-12 15:16:27 +0530

    improved UI a bit and defined the flow of app

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+//get a inner content value on btn click and display it
+
+//get a operator value and display it
+
+//write the functions of all operators
+
+//clear the display
+
+//= execute the math
+
+//backspace functionality
