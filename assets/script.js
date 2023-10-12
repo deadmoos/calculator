89847301adbca24ecf5a0aea8f99365fb91dc93c
deadmoos <72154654+deadmoos@users.noreply.github.com>
2023-10-12 23:57:15 +0530

    setup logic to click a btn and get its value displayed

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,12 @@
-//get a inner content value on btn click and display it
+//loop through all the input buttons and get the textContent on click//assign these values to a variable
+const inputValue = document.querySelector(".num-btn");
+
+console.log(inputValue.value);
+
+inputValue.addEventListener("click", function () {
+  let outputValue = inputValue.value;
+  document.querySelector(".calc-display").textContent += outputValue;
+});
 
 //get a operator value and display it
 
