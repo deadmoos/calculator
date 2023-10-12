0649b9ae3a0961d06361b7ea7ecb5b30d4d6aac4
deadmoos <72154654+deadmoos@users.noreply.github.com>
2023-10-13 00:35:24 +0530

    Used even delegation to identify numeric button being pressed and get its value

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,17 @@
 //loop through all the input buttons and get the textContent on click//assign these values to a variable
-const inputValue = document.querySelector(".num-btn");
-
-console.log(inputValue.value);
-
-inputValue.addEventListener("click", function () {
-  let outputValue = inputValue.value;
-  document.querySelector(".calc-display").textContent += outputValue;
+const inputValue = document.querySelector(".calc-num");
+inputValue.addEventListener("click", function (e) {
+  if (e.target && e.target.matches("button.num-btn")) {
+    console.log(e.target.value);
+  }
 });
+
+// console.log(inputValue.value);
+
+// inputValue.addEventListener("click", function () {
+//   let outputValue = inputValue.value;
+//   document.querySelector(".calc-display").textContent += outputValue;
+// });
 
 //get a operator value and display it
 
